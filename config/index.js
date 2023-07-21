dbb8391fe22184f32b23682731dd29f927f42f5c
MD RASHID ALI <rashid.zeyad2u@gmail.com>
2023-07-22 02:55:10 +0530

    send response function added for sending the response

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@
 const commonVariables = {
     APP_PORT: '7000',
     MONGODB_CON_STRING: 'mongodb://localhost/innova-tech-client',
+    STATUS: [200, 500, 400, 401],
 }
 
 //setting the common variables
