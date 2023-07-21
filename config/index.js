ffef2644b8166dcfae0d154555d3b4ada6c7878a
MD RASHID ALI <rashid.zeyad2u@gmail.com>
2023-07-22 02:48:12 +0530

    mongoDB connection added

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 // Common Environment Variables
 const commonVariables = {
     APP_PORT: '7000',
+    MONGODB_CON_STRING: 'mongodb://localhost/innova-tech-client',
 }
 
 //setting the common variables
